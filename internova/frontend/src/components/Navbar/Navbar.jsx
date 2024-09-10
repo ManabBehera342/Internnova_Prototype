21cfe9cf9a812701b8feaa7f56fca2d5300d2537
srutiprusty <sruti.prusty.cse.2022@gmail.com>
2024-09-11 00:03:20 +0530

    styling

--- internova/frontend/src/components/Navbar/Navbar.jsx
+++ internova/frontend/src/components/Navbar/Navbar.jsx
@@ -1,41 +1,3 @@
-/* import React, { useState, useEffect } from "react";
-import { Link } from "react-scroll";
-import './Navbar.css';
-
-const Navbar = () => {
-  const [sticky, setSticky] = useState(false);
-
-  useEffect(() => {
-
-    window.addEventListener('scroll', () => {
-      window.scrollY>50?setSticky(true):setSticky(false);
-    })
-  }, []);
-
-  return (
-    
-    <nav className={`container ${sticky?'dark-nav':''}`}>
-      <div className="logo"><img src="./images/logo.png" alt="logo" className='logo'/>
-      </div>
-        
-      <ul>
-                  <li><Link to='landing' smooth={true} offset={0} duration={500}>HOME</Link></li>
-                  <li><Link to='about' smooth={true} offset={-260} duration={500}>ABOUT</Link></li>
-                  <li><Link to='faq' smooth={true} offset={-150} duration={500}>FAQ</Link></li>
-               
-                <button className="login"><Link to='landing' smooth={true} offset={0} duration={500}>LOGIN</Link></button>
-                
-                <button className="signup"><Link to='landing' smooth={true} offset={0} duration={500}>SIGNUP</Link></button>
-                </ul>
-               
-
-    </nav>
-  );
-};
-
-export default Navbar;
- */
-
 
 import React, { useState, useEffect } from "react";
 import { Link } from "react-scroll";
@@ -69,8 +31,8 @@ const Navbar = () => {
         <li><Link to="about" smooth={true} offset={-60} duration={500}>ABOUT</Link></li>
         <li><Link to="faq" smooth={true} offset={500} duration={500}>FAQ</Link></li>
         <li className="auth-buttons">
-          <button className="login"><Link to="login" smooth={true} offset={0} duration={500}>LOGIN</Link></button>
-          <button className="signup"><Link to="signup" smooth={true} offset={0} duration={500}>SIGNUP</Link></button>
+          <button className="login">LOGIN</button>
+          <button className="signup">SIGNUP</button>
         </li>
       </ul>
 
