25b0ef6476009362dee65f79b2943cc8da93fb81
srutiprusty <sruti.prusty.cse.2022@gmail.com>
2024-10-14 22:40:55 +0530

    home

--- internova/frontend/src/components/Home/Home.jsx
+++ internova/frontend/src/components/Home/Home.jsx
@@ -1,18 +1,34 @@
 import React from 'react'
+import Navbar from './../Navbar/Navbar';
+import "./Home.css"
 
 const Home = () => {
   return (
     <>
-    <div className='container' id='home'>
-    {/* Gradient Background Elements */}
-    <div className="yellow-gradient1" />
-    
+     <div className="home-nav"><Navbar/></div>
+     <div className="home-page" id='home'>
 
-    {/* About Section */}
-    <div className='home'>
-      <h1 className='home-head'>home</h1>
-      </div>
-      </div>
+        <div className='home-container'  >
+          {/* <h1 className='home-head'>home</h1> */}
+          <div className="leftSec">
+            <h1 className='home-head'>Spotting talent, shaping futures</h1>
+            <div className="search-bar">
+            <input
+              placeholder='Search what you want...'
+              type='text'
+              value={""}
+              onChange={""}
+              />
+            </div>
+    
+     {/*  <HiLocationMarker color="var(--blue)" size={25} /> */}
+            <p className='popular'>Popular : Full Stack Web Developer,UI/UX Designer,ML Engineer,....</p>
+          </div>
+          <div className="head-sec">
+            <img src="./images/Group 10.png" alt="home" width={500} height={550}/>
+          </div>
+        </div>
+     </div>
     </>
   )
 }
