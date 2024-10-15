3c5cca04fd1784ae436df9a23cd001552c183d25
srutiprusty <sruti.prusty.cse.2022@gmail.com>
2024-10-15 17:45:45 +0530

    responsive

--- internova/frontend/src/components/Home/Home.jsx
+++ internova/frontend/src/components/Home/Home.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+/* import React from 'react'
 import Navbar from './../Navbar/Navbar';
 import "./Home.css"
 import { FaSearch } from 'react-icons/fa';
@@ -11,7 +11,7 @@ const Home = () => {
      <div className="home-page" id='home'>
 
         <div className='home-container'  >
-          {/* <h1 className='home-head'>home</h1> */}
+          
           <div className="leftSec">
             <h1 className='home-head'>Spotting talent, shaping futures</h1>
             <div className="search-bar">
@@ -24,7 +24,7 @@ const Home = () => {
               <FaSearch/>
             </div>
     
-     {/*  <HiLocationMarker color="var(--blue)" size={25} /> */}
+    
             <p className='popular'>Popular : Full Stack Web Developer,UI/UX Designer,ML Engineer,....</p>
           </div>
           <div className="head-sec">
@@ -37,3 +37,54 @@ const Home = () => {
 }
 
 export default Home
+ */
+
+
+
+import React, { useState } from 'react';
+import Navbar from './../Navbar/Navbar';
+import './Home.css';
+import { FaSearch } from 'react-icons/fa';
+
+
+const Home = () => {
+  // Using state to control the input field
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const handleInputChange = (e) => {
+    setSearchTerm(e.target.value);
+  };
+
+  return (
+    <>
+      <div className="home-nav">
+        <Navbar />
+      </div>
+      <div className="home-page" id="home">
+        <div className="home-container">
+          <div className="leftSec">
+            <h1 className="home-head">Spotting talent, shaping futures</h1>
+            <div className="search-bar">
+              <input
+                placeholder="Search what you want..."
+                type="text"
+                value={searchTerm}
+                onChange={handleInputChange}
+              />
+              <FaSearch />
+            </div>
+            <p className="popular">
+              Popular : Full Stack Web Developer, UI/UX Designer, ML Engineer,....
+            </p>
+          </div>
+          <div className="head-sec">
+            {/* Use the imported image reference */}
+            <img src="./images/Group 10.png" alt="home" width={500} height={550} />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Home;
