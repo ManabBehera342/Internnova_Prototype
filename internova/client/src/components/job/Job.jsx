33839fa3201662f5a0a421e01b5666298f64cbc9
srutiprusty <sruti.prusty.cse.2022@gmail.com>
2024-12-12 12:39:51 +0530

    tried

--- internova/client/src/components/job/Job.jsx
+++ internova/client/src/components/job/Job.jsx
@@ -41,9 +41,16 @@ const Job = ({ job }) => {
         </div>
       </div>
 
+      {/*   <div>
+        <h1 className="font-bold text-lg my-2">{job?.title}</h1>
+        <p className="text-sm text-gray-600">{job?.description.trim()}</p>
+      </div> */}
       <div>
         <h1 className="font-bold text-lg my-2">{job?.title}</h1>
-        <p className="text-sm text-gray-600">{job?.description}</p>
+        <p className="text-sm text-gray-600">
+          {job?.description?.split(" ").slice(0, 29).join(" ")}
+          {job?.description?.split(" ").length > 50 ? "..." : ""}
+        </p>
       </div>
       <div className="flex items-center gap-2 mt-4">
         <Badge className={"text-blue-700 font-bold"} variant="ghost">
--- internova/server/controllers/recommendation.controller.js
+++ internova/server/controllers/recommendation.controller.js
@@ -1,34 +1,5 @@
 import axios from "axios";
 
-/* export const getJobRecommendations = async (req, res) => {
-  try {
-    const { userId } = req.body;
-
-    // Call the ML API
-    const response = await axios.post(
-      "https://internova-api.onrender.com/match_jobs",
-      {
-        userId,
-      }
-    );
-
-    // Process and filter recommendations if needed
-    const recommendations = response.data;
-
-    res.status(200).json({
-      success: true,
-      recommendations,
-    });
-  } catch (error) {
-    console.error("Error in job recommendations:", error);
-    res.status(500).json({
-      success: false,
-      message: "Error fetching job recommendations",
-      error: error.message,
-    });
-  }
-}; */
-
 export const getJobRecommendations = async (req, res) => {
   try {
     const { userId } = req.body;
