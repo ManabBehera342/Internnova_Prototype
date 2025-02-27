b0a14fc96c83e2c511f7e50db17d6c01dd36ca04
srutiprusty <sruti.prusty.cse.2022@gmail.com>
2025-02-27 12:05:22 +0530

    client

--- internova/client/src/utils/constant.js
+++ internova/client/src/utils/constant.js
@@ -1,4 +1,4 @@
-/* export const USER_API_END_POINT =
+export const USER_API_END_POINT =
   "https://internnova-server.vercel.app/api/v1/user";
 export const JOB_API_END_POINT =
   "https://internnova-server.vercel.app/api/v1/job";
@@ -8,8 +8,8 @@ export const COMPANY_API_END_POINT =
   "https://internnova-server.vercel.app/api/v1/company";
 export const RECOMMENDATION_API_END_POINT =
   "https://internnova-server.vercel.app/api/v1/recommendations";
- */
 
+/* 
 export const USER_API_END_POINT = "http://localhost:4000/api/v1/user";
 export const JOB_API_END_POINT = "http://localhost:4000/api/v1/job";
 export const APPLICATION_API_END_POINT =
@@ -17,3 +17,4 @@ export const APPLICATION_API_END_POINT =
 export const COMPANY_API_END_POINT = "http://localhost:4000/api/v1/company";
 export const RECOMMENDATION_API_END_POINT =
   "http://localhost:4000/api/v1/recommendations";
+ */
