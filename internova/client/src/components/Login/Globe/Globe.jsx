f5eda04d4fa47b250d69428bcea6d16653fb3d84
srutiprusty <sruti.prusty.cse.2022@gmail.com>
2025-04-17 15:40:45 +0530

    globe fixed

--- internova/client/src/components/Login/Globe/Globe.jsx
+++ internova/client/src/components/Login/Globe/Globe.jsx
@@ -1,4 +1,3 @@
-
 import React, { useEffect, useRef } from "react";
 import createGlobe from "cobe";
 import "./Globe.css";
@@ -14,7 +13,7 @@ const Globe = () => {
     const globe = createGlobe(canvasRef.current, {
       devicePixelRatio: window.devicePixelRatio,
       width: canvasRef.current.offsetWidth, // Canvas width based on parent size
-      height: canvasRef.current.offsetHeight, // Canvas height based on parent size
+      height: canvasRef.current.offsetHeight / 1.5, // Canvas height based on parent size
       phi: Math.PI / 2, // Initial angle
       theta: 0.3, // Tilt angle
       dark: 1, // Darkness level
@@ -26,7 +25,7 @@ const Globe = () => {
       glowColor: [0, 0, 0], // Glow color
       markers: [
         { location: [37.7749, -122.4194], size: 0.05 }, // San Francisco
-        { location: [40.7128, -74.0060], size: 0.07 }, // New York
+        { location: [40.7128, -74.006], size: 0.07 }, // New York
         { location: [51.5074, -0.1278], size: 0.05 }, // London
       ],
       onRender: (state) => {
